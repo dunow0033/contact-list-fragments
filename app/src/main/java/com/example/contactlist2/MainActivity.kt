2ac4187540455f6345186f0680675bdee2288db0
davidacosta25081 <42393043+davidacosta25081@users.noreply.github.com>
2021-08-22 12:37:12 -0400

    Using Fragments and RecyclerView

---
+++ app/src/main/java/com/example/contactlist2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.contactlist2
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.contactlist2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+
+
+
+
+}
